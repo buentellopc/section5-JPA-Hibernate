468fd4ff5718e093596c13661703cff35158facd
buentellopc <buentellopc@com>
2022-08-12 15:39:30 -0500

    Set proper dependency injections and implementation to run the program

--- src/main/java/com/in28minutes/jpa/hibernate/demo/Section5In28minutesApplication.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/Section5In28minutesApplication.java
@@ -1,12 +1,21 @@
 package com.in28minutes.jpa.hibernate.demo;
 
+import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+
 // remember to implement command line runner
+// remember to specify an entity manager class with the annotation of repository
+
 @SpringBootApplication
 public class Section5In28minutesApplication implements CommandLineRunner {
+    @Autowired
+    private CourseRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(Section5In28minutesApplication.class, args);
@@ -14,6 +23,6 @@ public class Section5In28minutesApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        Course course = repository.findById(10001L);
     }
 }
--- src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
@@ -2,9 +2,11 @@ package com.in28minutes.jpa.hibernate.demo.repository;
 
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
+@Repository
 public class CourseRepository {
 
     @Autowired
