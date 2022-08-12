cba678e90b988b8edd72f563b7a8098208f83f40
buentellopc <buentellopc@com>
2022-08-12 15:21:21 -0500

    Crete JPA Entity Course

---
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/Section5In28minutesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.jpa.hibernate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section5In28minutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section5In28minutesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/Section5In28minutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.jpa.hibernate.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section5In28minutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
