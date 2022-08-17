8b9499c17333c137c5cabbfe6641d5eb717f6b5e
buentellopc <buentellopc@gmail.com>
2022-08-17 12:54:51 -0500

    [step13] entity manager methods - refresh

--- src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepository.java
@@ -51,15 +51,24 @@ public class CourseRepository {
             Course course1 = new Course("new course, playing with this");
             Course course2 = new Course("angular js in 100 steps");
 
-//            persist the courses (entity manager will now keep track of any changes)
+//            persist (fire hibernate sequence to add an id) the courses (entity manager will now keep track of any changes)
             em.persist(course1);
             em.persist(course2);
+
+//            save changes to database before end of transaction
             em.flush();
 
-//            entity manager tracking for course2 will be cleared, the update of course 2 should not be visible
-            em.detach(course2);
+//            change both courses names
             course1.setName("updating the same course, without explicitly saving it!");
             course2.setName("angular js in 100 steps - updated");
+
+//            retrieve the actual value of course1 from database
+//            any new changes will be lost
+//            em.refresh(course1);
+
+//            course2 will have the new changes saved
+            em.flush();
+
         }
 
 
